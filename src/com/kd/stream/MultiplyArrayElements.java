ff27e10970c21cef341d294720cbe98faf0afe73
Dharmanand <Dharmanand@users.noreply.github.com>
2023-03-07 19:48:13 +0530

    Update MultiplyArrayElements.java

--- src/com/kd/stream/MultiplyArrayElements.java
+++ src/com/kd/stream/MultiplyArrayElements.java
@@ -1,16 +1,20 @@
 package com.kd.stream;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 public class MultiplyArrayElements {
 
 	public static void main(String[] args) {
 
-		Integer[] arr = {2, 5, 18, 45, 34};
-		
-		int multiply = Arrays.stream(arr).reduce((a,b) -> a*b).get();
-		System.out.println(multiply);
-		
+		Integer[] arr = { 2, 5, 18, 45, 34 };
+
+		Optional<Integer> multiplication = Arrays.stream(arr).reduce((a, b) -> a * b);
+
+		if (multiplication.isPresent())
+			System.out.println(multiplication.get());
+		// output : 275400
+
 	}
 
 }

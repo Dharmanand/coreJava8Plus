ea83c2ba9f5568a940d038dd4b6d091bb9e9791f
Dharmanand <kumar.kd20@gmail.com>
2023-03-28 06:14:34 +0530

    Print 2D array diagonally

---
+++ src/com/kd/ds/array/twoD/DiagonalPrint.java
@@ -0,0 +1,33 @@
+package com.kd.ds.array.twoD;
+
+import java.util.Arrays;
+import java.util.Scanner;
+//https://www.youtube.com/watch?v=lvRdFCMD_Ew&list=PL-Jc9J83PIiFkOETg2Ybq-FMuJjkZSGeH&index=15
+public class DiagonalPrint {
+
+	public static void main(String[] args) {
+
+
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+//		int n = sc.nextInt();
+		
+		int[][] arr = new int[m][m];
+		
+		for(int i=0; i < m; i++)
+			for(int j=0; j < m; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		sc.close();
+		
+		for(int i=0; i < m; i++)
+			System.out.println(Arrays.toString(arr[i]));
+		
+		for(int g=0; g<m;g++) {
+			for(int i=0, j=g; j<m; i++, j++) {
+				System.out.println(arr[i][j]);
+			}
+		}
+	}
+
+}
